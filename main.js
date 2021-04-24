611ece88243173212f852228f4ec719e241783ab
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-24 18:31:43 -0500

    commit tree 23f42369db3ac469f4c8ef6b63a7bac1fdebfa04

--- main.js
+++ main.js
@@ -3,10 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const err = document.getElementById('modal')
+err.className = "hidden"
+
+const hearts = document.getElementsByClassName('like-glyph')
+
+for (let i in hearts) {
+  hearts[i].addEventListener('click', function(e){
+    mimicServerCall()
+    .then(activateHeart(e))
+    .catch(error => {
+      err.className = ""
+      err.children[1].innerText = error.message
+      setTimeout(function(){err.className = "hidden"}, 5000)
+    })
+  })
+}
 
 
-
-
+function activateHeart(e) {
+  e.target.innerText = FULL_HEART
+  e.target.className = 'actived-heart'
+  e.target.addEventListener('click', function(e){
+    e.preventDefault()
+    e.target.className = ''
+    e.target.innerText = EMPTY_HEART
+    e.target.addEventListener('click', function(e){
+      mimicServerCall()
+      .then(activateHeart(e))
+      .catch(error => {
+        err.className = ""
+        err.children[1].innerText = error.message
+        setTimeout(function(){err.className = "hidden"}, 5000)
+      })
+    })
+  })
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
